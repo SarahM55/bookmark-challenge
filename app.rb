48289440a8edb8336f6a1ebda15c3f7ecf058f3a
Charlotte Buchan <cebuc@talktalk.net>
2022-02-14 16:09:12 +0000

    sort out errors on viewing bookmakrs

--- app.rb
+++ app.rb
@@ -8,7 +8,17 @@ class BookmarkManager < Sinatra::Base
 
   get '/' do
     "Bookmark Manager"
+    erb :index
   end
+
+  get '/bookmarks' do
+    bookmarks = [
+      "http://www.google.com"
+    ]
+    erb :index
+
+  end
+
 
   run! if app_file == $0
 end
--- spec/feature/feature_spec.rb
+++ spec/feature/feature_spec.rb
@@ -4,5 +4,11 @@ feature "homepage" do
   scenario "visiting the index page" do
     visit ('/')
     expect(page).to have_content "Bookmark Manager"
+  end  
+end
+feature "show bookmark page" do 
+  scenario "user can view bookmarks"  do 
+    visit ('/bookmarks')
+    expect(page).to have_content "http://www.google.com"
   end
 end
