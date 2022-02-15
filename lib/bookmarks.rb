d5ac42e033deb47aa6543348dfc2e236f74b90f6
Sarah Moffat <sarah.moffat5@gmail.com>
2022-02-15 15:30:46 +0000

    Connected to database - step 7

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -1,9 +1,11 @@
+require 'pg'
+
 class Bookmark
 
   def self.all
-    [
-      "http://www.google.com"
-
-    ]
+    connection = PG.connect(dbname: 'bookmark_manager')
+    result = connection.exec("SELECT * from bookmarks;")
+    result.map { |bookmark| bookmark['url'] }
   end
+
 end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,10 +1,11 @@
 require_relative '../lib/bookmarks.rb'
+require 'pg'
 
 describe Bookmark do
   describe '.all' do
     it "returns all bookmarks" do 
       bookmarks = Bookmark.all
-      expect(bookmarks).to include("http://www.google.com")
+      expect(bookmarks).to include("http://www.google.com/")
     end
   end
 end
